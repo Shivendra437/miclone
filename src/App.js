1738bafcfed6b59eb751dda218f2c3f361747413
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2022-11-30 21:42:24 +0530

    project update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js"
-import {  BrowserRouter as Router,Route } from "react-router-dom"
+import {  BrowserRouter as Router,Route ,Routes} from "react-router-dom"
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
 import Offers from "./components/Offers.js"
@@ -33,28 +33,26 @@ function App() {
        <StarProduct starProduct={data.starProduct} />
        <Heading text="HOT ACCESSORIES" />
        <HotAccessoriesMenu />
+<Routes>
+       <Route exact path="/music" element={
+       <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />}/>
 
-       <Route exact path="/music">
-       <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />
-       </Route>
+       <Route exact path="/smartDevice" element={
+       <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />}/>
+      
 
-       <Route exact path="/smartDevice">
-       <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />
-       </Route>
+       <Route exact path="/home"element={
+       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />}/>
+     
 
-       <Route exact path="/home">
-       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />
-       </Route>
+       <Route exact path="/lifestyle"element={
+       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />}/>
+     
+       <Route exact path="/mobileAccessories"element={
+      < HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}/>}/>
 
-       <Route exact path="/lifestyle">
-       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />
-       </Route>
-
-
-       <Route exact path="/mobileAccessories">
-       <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />
-       </Route>
-
+       
+</Routes>
        <Heading text="PRODUCT REVIEWS"/>
 
        <ProductReviews productReviews={data.productReviews}/>
